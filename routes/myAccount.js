597ec26bdfccd3b1acbfe26c35bc8bb0da13d569
Jace Galloway <jace.galloway@gmail.com>
2025-09-16 22:03:23 -0400

    third test

--- routes/myAccount.js
+++ routes/myAccount.js
@@ -16,6 +16,7 @@ router.post('/', auth, async (req, res) => {
 			user.name = name;
 			await user.save();
 			res.json(user ? user : null);
+			return;
 		} else if (type === 2) {
 			const user = await User.findOne({
 				user: req.user._id,
@@ -30,22 +31,27 @@ router.post('/', auth, async (req, res) => {
 			user.password = newPassword;
 			await user.save();
 			res.json(user ? user : null);
+			return;
 		} else if (type === 3) {
 			await flaghistories.deleteMany({
 				creatorId: req.user._id,
 			});
 			await dealbreakers.deleteMany({
-				user: req.user.id,
+				user: req.user._id,
 			});
 			await User.deleteOne({
 				_id: req.user._id,
 			});
 			res.json({ message: 'Account deleted' });
+			return;
 		}
 		res.status(500).json({ message: 'Invalid type' });
+		return;
 	} catch (error) {
 		res.status(500).json({ message: error.message });
+		return;
 	}
+	return;
 });
 
 module.exports = router;
